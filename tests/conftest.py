5776f3fb8cfb172ecd5bab04e528e6b7dcf9ca4e
Houssem Guissouma <houssem.guissouma@kit.edu>
2025-04-19 15:45:13 +0200

    Fix tests partly

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,40 @@
 import os
+from pathlib import Path
 
 import pytest
 
 
 @pytest.fixture
-def test_dir() -> str:
-    """Returns the full path of the test directory."""
-    return os.path.dirname(__file__)
+def test_dir() -> Path:
+    """Returns the full path of the test directory as a Path object."""
+    return Path(__file__).parent
 
 
 @pytest.fixture
-def test_export_dir(test_dir: str) -> str:
-    """Returns the path of the test export directory.
-
-    Args:
-        test_dir_path: The path of the test directory.
-    """
-    export_dir = os.path.join(test_dir, "_export")
-    os.makedirs(export_dir, exist_ok=True)
+def test_export_dir(test_dir: Path) -> Path:
+    """Returns the path of the test export directory as a Path object."""
+    export_dir = test_dir / "_export"
+    export_dir.mkdir(exist_ok=True)
     return export_dir
+
+
+@pytest.fixture
+def root_path(test_dir: Path) -> Path:
+    """Returns the root path of the FigExport project."""
+    return test_dir.parent
 
 
 @pytest.fixture
 def plantuml_url() -> str:
     """Returns the URL of the PlantUML JAR file."""
-    return "https://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
+    return "https://github.com/plantuml/plantuml/releases/download/v1.2025.2/plantuml-1.2025.2.jar"
+
+
+@pytest.fixture
+def drawio_path() -> str:
+    """Returns the path of the draw.io executable."""
+    # For Windows, you can use the following path:
+    if os.name == 'nt':
+        return "C:/Program Files/draw.io/draw.io.exe"
+    else:
+        return "/snap/bin/drawio"
--- tests/one_file_export_test.py
+++ tests/one_file_export_test.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import pytest
 
@@ -16,8 +17,8 @@ class TestFileExport:
     @pytest.mark.parametrize("export_format", 
                              [ExportFormat.PDF, ExportFormat.SVG, 
                               ExportFormat.PNG, ExportFormat.JPG])
-    def test_svg_export(self, test_dir: str, 
-                        test_export_dir: str, export_format: ExportFormat) -> None:
+    def test_svg_export(self, test_dir: Path, 
+                        test_export_dir: Path, export_format: ExportFormat) -> None:
         """Tests the conversion of an SVG file to multiple formats.
 
         Args:
@@ -25,7 +26,7 @@ class TestFileExport:
             test_export_dir: The path of the test export directory.
             export_format: The output format being tested.
         """
-        svg_figure_path = os.path.join(test_dir, "test_folder1", "Rectangle1_svg.svg")
+        svg_figure_path = test_dir / "test_folder1" / "Rectangle1_svg.svg"
 
         # Convert the input SVG file
         svg_exporter = SvgExporter(export_format)
@@ -38,42 +39,21 @@ class TestFileExport:
     @pytest.mark.parametrize("export_format", 
                              [ExportFormat.PDF, ExportFormat.SVG,
                               ExportFormat.PNG, ExportFormat.JPG])
-    def test_puml_export(self, 
-                         plantuml_url: str, 
-                         test_dir: str, 
-                         test_export_dir: str,
-                         export_format: ExportFormat) -> None:
-        """Tests the conversion of a PlantUML file to multiple formats.
-
-        Args:
-            test_dir: The path of the test directory.
-            test_export_dir: The path of the test export directory.
-        """
-        puml_figure_path = os.path.join(test_dir, "test_folder1", "Sequence1_puml.puml")
-
-        jar_path = os.path.join(test_dir, "plantuml.jar")
-        puml_exporter = PumlExporter(export_format, jar_path, plantuml_url)
-        output_file = puml_exporter.export(puml_figure_path, test_export_dir)
-
-        # Assert that the exported file is valid
-        assert is_valid_file(output_file, export_format.value), \
-               f"Export failed for format: {export_format}"
-        
-    @pytest.mark.parametrize("export_format", 
-                             [ExportFormat.PDF, ExportFormat.SVG,
-                              ExportFormat.PNG, ExportFormat.JPG])
-    def test_drawio_export(self, test_dir: str, 
-                           test_export_dir: str, export_format: ExportFormat) -> None:
+    def test_drawio_export(self, 
+                           drawio_path: str,
+                           test_dir: Path,
+                           test_export_dir: Path,
+                           export_format: ExportFormat) -> None:
         """Tests the conversion of a Drawio file to multiple formats.
 
         Args:
             test_dir: The path of the test directory.
             test_export_dir: The path of the test export directory.
         """
-        drawio_figure_path = os.path.join(test_dir, "test_folder1", "Rectangle1_drawio.drawio")
+        drawio_figure_path = test_dir / "test_folder1" / "Rectangle1_drawio.drawio"
 
         # Convert the input Drawio file
-        drawio_exporter = DrawioExporter(export_format)
+        drawio_exporter = DrawioExporter(drawio_path, export_format)
         output = drawio_exporter.export(drawio_figure_path, test_export_dir)
 
         # Assert that the exported file(s) are valid
@@ -88,19 +68,44 @@ class TestFileExport:
     @pytest.mark.parametrize("export_format",
                              [ExportFormat.PDF, ExportFormat.SVG,
                               ExportFormat.PNG, ExportFormat.JPG])
-    def test_tikz_export(self, test_dir: str, 
-                         test_export_dir: str, export_format: ExportFormat) -> None:
+    def test_tikz_export(self, 
+                         test_dir: Path, 
+                         test_export_dir: Path, export_format: ExportFormat) -> None:
         """Tests the conversion of a TikZ file to an SVG file.
 
         Args:
             test_dir: The path of the test directory.
             test_export_dir: The path of the test export directory.
         """
-        tikz_figure_path = os.path.join(test_dir, "test_folder1", "Rectangle1_tikz.tex")
+        tikz_figure_path = test_dir / "test_folder1" / "Rectangle1_tex.tex"
 
         # Convert the input Drawio file
         tikz_exporter = TexExporter(export_format)
         output_file = tikz_exporter.export(tikz_figure_path, test_export_dir)
+
+        # Assert that the exported file is valid
+        assert is_valid_file(output_file, export_format.value), \
+               f"Export failed for format: {export_format}"
+        
+    @pytest.mark.parametrize("export_format", 
+                            [ExportFormat.PDF, ExportFormat.SVG,
+                            ExportFormat.PNG, ExportFormat.JPG])
+    def test_puml_export(self, 
+                         plantuml_url: str, 
+                         test_dir: Path, 
+                         test_export_dir: Path,
+                         export_format: ExportFormat) -> None:
+        """Tests the conversion of a PlantUML file to multiple formats.
+
+        Args:
+            test_dir: The path of the test directory.
+            test_export_dir: The path of the test export directory.
+        """
+        puml_figure_path = test_dir / "test_folder1" / "Sequence1_puml.puml"
+
+        jar_path = os.path.join(test_dir, "plantuml.jar")
+        puml_exporter = PumlExporter(jar_path, plantuml_url, export_format)
+        output_file = puml_exporter.export(puml_figure_path, test_export_dir)
 
         # Assert that the exported file is valid
         assert is_valid_file(output_file, export_format.value), \
